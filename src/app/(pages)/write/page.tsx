449c9e7464e33a3da6adbda2fd310f72f4d463f7
Leewonno <dldnjssh123@naver.com>
2025-11-02 23:13:56 +0900

    feat : unauthorized

--- src/app/(pages)/write/page.tsx
+++ src/app/(pages)/write/page.tsx
@@ -1,3 +1,4 @@
+import { Unauthorized } from "@/shares";
 import { WriteEditor } from "@/widgets";
 
 export default async function BlogPostCreate() {
@@ -6,9 +7,9 @@ export default async function BlogPostCreate() {
 
   if (process.env.NODE_ENV !== 'development') {
     return (
-      <>권한이 없습니다.</>
+      <Unauthorized />
     )
-  } 
+  }
 
   // 테스트용 인위적 지연
   await new Promise((resolve) => setTimeout(resolve, 1500));
--- src/shares/index.ts
+++ src/shares/index.ts
@@ -2,6 +2,7 @@
 export { SvgIcon } from './components'
 export { MarkdownViewer } from './components'
 export { ScrollToTop } from './components'
+export { Unauthorized } from './components'
 
 // ui
 export { Layout } from './ui'
